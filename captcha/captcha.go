9d78c052e07cb5a00c007509c0f3808c56e06079
Peerapat Naksumpan <peerapath.t@gmail.com>
2019-10-24 09:06:22 +0700

    captcha: handle case format is 1

--- captcha/captcha.go
+++ captcha/captcha.go
@@ -1,5 +1,9 @@
 package captcha
 
 func Generate(format, leftOperand, operator, rightOperand string) string {
+	if format == "1" {
+		return "one + 1"
+	}
+
 	return "1 + one"
 }
--- captcha/captcha_test.go
+++ captcha/captcha_test.go
@@ -11,4 +11,8 @@ func TestGenerate(t *testing.T) {
 	t.Run("should return left operand as-is and right operand as text when format is 0", func(t *testing.T) {
 		assert.Equal(t, "1 + one", captcha.Generate("0", "1", "0", "1"))
 	})
+
+	t.Run("should return left operand as text and right operand as-is when format is 1", func(t *testing.T) {
+		assert.Equal(t, "one + 1", captcha.Generate("1", "1", "0", "1"))
+	})
 }
