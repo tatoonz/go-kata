517ba660200307ee7db30a171eba2672e2c63c2f
TaToonz <peerapath.t@gmail.com>
2019-10-25 10:14:12 +0700

    captcha: use map instead of switch for get operator sign

--- captcha/captcha.go
+++ captcha/captcha.go
@@ -1,18 +1,19 @@
 package captcha
 
-import "fmt"
+import (
+	"fmt"
+)
+
+var operatorSignMap = map[string]string{
+	"0": "+",
+	"1": "-",
+	"2": "*",
+}
 
 func Generate(format, leftOperand, operator, rightOperand string) string {
-	operatorSign := "+"
-	if operator == "1" {
-		operatorSign = "-"
-	} else if operator == "2" {
-		operatorSign = "*"
-	}
-
 	if format == "1" {
-		return fmt.Sprintf("one %s 1", operatorSign)
+		return fmt.Sprintf("one %s 1", operatorSignMap[operator])
 	}
 
-	return fmt.Sprintf("1 %s one", operatorSign)
+	return fmt.Sprintf("1 %s one", operatorSignMap[operator])
 }
