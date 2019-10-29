3c92edd3b1c5abdc767cdd9bd629e5781db13f4e
TaToonz <peerapath.t@gmail.com>
2019-10-29 21:23:13 +0700

    captcha: change type of leftOperand argument from string to int

--- captcha/captcha.go
+++ captcha/captcha.go
@@ -17,10 +17,10 @@ func (op operand) Word() string {
 	return operandWords[i]
 }
 
-func Generate(format int, leftOperand string, operator int, rightOperand string) string {
+func Generate(format int, leftOperand int, operator int, rightOperand string) string {
 	if format == 1 {
-		return fmt.Sprintf("%s %s %s", operand(leftOperand).Word(), operatorSigns[operator], rightOperand)
+		return fmt.Sprintf("%s %s %s", operandWords[leftOperand], operatorSigns[operator], rightOperand)
 	}
 
-	return fmt.Sprintf("%s %s %s", leftOperand, operatorSigns[operator], operand(rightOperand).Word())
+	return fmt.Sprintf("%s %s %s", strconv.Itoa(leftOperand), operatorSigns[operator], operand(rightOperand).Word())
 }
